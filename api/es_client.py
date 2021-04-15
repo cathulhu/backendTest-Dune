0fb97186f82c5a80109ed5886f641465d9018a53
cathulhu <iheartpugs89@gmail.com>
2021-04-15 03:49:11 -0700

    added system to automatically download locations, fuzzy location matching, and fixed crash on no results, as well as minor UI features

---
+++ api/es_client.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python3
+import sys
+import json
+import pprint
+from elasticsearch import Elasticsearch
+
+print("These were the paramters passed to esclient.py: \n")
+print(sys.argv)
+
+#!/usr/bin/env python3
+
+#Sys needed for arguments
+import sys
+
+#JSON needed to process incoming Elasticsearch results
+#and for output formatting for easier use by webserver.
+import json
+
+#ElasticSearch API is needed (alternatively commands could be manually written
+#with REST Python API) for interaction with Fermilab's ElasticSearch system
+from elasticsearch import Elasticsearch
+
+from datetime import datetime
+
+#Argparse will be used for proper argument handling in the long-term.
+#import argparse as ap
+
+#Started code for future use.
+#parser = ap.ArgumentParser()
+#parser.add_argument()
+
+#Hard-coded value for what we think we remember the connection speed
+#being. Used to calculate our network use percentage.
+#Needs refining
+max_speed = 100000000000 #100 gb/s
+
+
+#How many search results we want to return.
+#Capping search results at 2,500 since API documentation recommends keeping searches
+#relatively small and stepping through with search_after instead of using massive search
+#volumes and scroll.
+search_size = 2500
+
+error_out = {
+    "name" : "ERROR",
+    "source" : "ERROR",
+    "destination" : "ERROR",
+    "file_size" : 0,
+    "start_time" : "1970-01-01 00:00:00",
+    "file_transfer_time" : "0.0",
+    "transfer_speed(b/s)" : "0.0",
+    "transfer_speed(MB/s)" : "0.0",
+    "max_usage_percentage" : "0.0"
+}
+
+#Last supplied command line argument should be a date
+#of the form yyyy/mm/dd
+y,m,d = sys.argv[-1].split('/')
+
+
+today = datetime.today()
+target_date = datetime.strptime(sys.argv[-1], "%Y/%m/%d")
+
+#Hardcoded output file name
+output_file = "out.json"
+
+
+if target_date > today:
+    print("Error: Cannot read data from future dates")
+    f = open(output_file, "w+")
+    f.write(json.dumps({"data" : error_out}, indent=2))
+    f.close()
+    exit()
+
+#URL of the DUNE Elasticsearch cluster
+es_cluster = "https://fifemon-es.fnal.gov"
+
+#Index for the specified month
+index = f"rucio-transfers-v0-{y}.{m}"
+
+#Makes the Elasticsearch client
+client = Elasticsearch([es_cluster])
+
+#Search template for Elasticsearch client
+#Still needs to be made to work with date range
+es_template = {
+    "query" : {
+        "bool" : {
+            "filter" : {
+                "range" : {
+                    "@timestamp" : {
+                        "gte" : f"{y}-{m}-{d}",
+                        "lte" : f"{y}-{m}-{d}"
+                    }
+                }
+            },
+            "must" : {
+                "match": {
+                     "event_type" : "transfer-done"
+                }
+            }
+        }
+    }
+}
+
+
+
+
+#End of initial variable and template setup
+
+#Beginning of function definitions
+
+#Function to compile all of our transfer info and speed info into a
+#dictionary for conversion to JSON and export.
+def compile_info(transfers, speed_info):
+    json_strings = []
+    #Compiles each dictionary
+    for i in range(len(transfers)):
+        new_json = {
+            "name": transfers[i]["_source"]["name"],
+            "source": transfers[i]["_source"]["src-rse"],
+            "destination": transfers[i]["_source"]["dst-rse"],
+            "file_size": transfers[i]["_source"]["file-size"],
+            "start_time": transfers[i]["_source"]["started_at"],
+            "file_transfer_time": str(speed_info[i]["file_transfer_time"]),
+            "transfer_speed(b/s)": str(speed_info[i]["transfer_speed(b/s)"]),
+            "transfer_speed(MB/s)": str(speed_info[i]["transfer_speed(MB/s)"]),
+            "max_usage_percentage": str(speed_info[i]["max_usage_percentage"])
+        }
+        #Appends it to the output list
+        json_strings.append(new_json)
+    #Returns the output list
+    return json_strings
+
+#Function to calculate the relevant times and speeds from each transfer
+#in our list of JSONS (which at this point have been converted to dictionaries)
+def get_speeds(transfers):
+    speed_info = []
+    to_remove = []
+    for transfer in transfers:
+        if transfer["_source"]["event_type"] != "transfer-done":
+            transfers.remove(transfer)
+            continue
+        #Try/except that was supposed to catch and remove the wrong types of
+        #JSONs. Didn't account for a LOT of potential issues like errors.
+        #Needs rewriting to handle those.
+        #Also pulls the (transfer request?) creation time
+        c_time = transfer["_source"]["created_at"]
+        #Pulls the (transfer request?) submission time, the transfer start time,
+        #and the transfer end time, as well as the file size
+        sub_time = transfer["_source"]["submitted_at"]
+        start_time = transfer["_source"]["started_at"]
+        fin_time = transfer["_source"]["transferred_at"]
+        f_size = float(transfer["_source"]["bytes"]) * 8
+
+        #Places our relevant times into an array for processing
+        time_arr = [c_time, sub_time, start_time, fin_time]
+        len_arr = []
+
+        #Finds the time differences for each set of times (creation to submission,
+        #submission to starting, and transmission start to transmission end)
+        #Initial time format is YYYY:M:DTH:M:SZ where T separates the year-month-day
+        #portion from the hours-minutes-second portion and the Z denotes UTC
+        for i in range(len(time_arr)-1):
+            #Gets our times from the JSON's format into a workable form
+            #First divides
+            split_1 = time_arr[i].split()
+            split_2 = time_arr[i + 1].split()
+            #Splits the hour-minute-second portion into individual pieces
+            #Note: In the future, with stupidly long transmissions, we may
+            #need to account for days, but I doubt it.
+            t_1 = split_1[1].split(':')
+            t_2 = split_2[1].split(':')
+
+            #Pulls the difference between each individual number set
+            #The hours, minutes, and seconds being greater in
+            #the start time than in the end time are accounted for later on
+            h_diff = float(t_2[0]) - float(t_1[0])
+            m_diff = float(t_2[1]) - float(t_1[1])
+            s_diff = float(t_2[2]) - float(t_1[2])
+
+            #Accounts for the hour being higher in the start time, which would
+            #mean that the day had turned over at some point.
+            if h_diff < 0:
+                h_diff += 24
+
+            #The difference in minutes and seconds being negative is accounted
+            #for here. The hour difference (thanks to the code above) should
+            #always be 0 or greater. If the minutes and seconds are different,
+            #then the hours should always be greater than zero, so the overall
+            #time will still be positive.
+            tot_time = (h_diff * 60 + m_diff) * 60 + s_diff
+
+            #Appends each time to the time length array
+            len_arr.append(tot_time)
+
+        #Calculates the transfer speed from the transfer start to end time and
+        #the file size
+        transfer_speed = f_size/len_arr[2]
+        #Filters out transfers with abnormally short transfer times
+        if len_arr[2] < 10.0 or len_arr[2] > 12 * 60 * 60:
+            to_remove.append(transfer)
+            continue
+        #Fills our speed information dictionary for this JSON object
+        info = {
+            "creation_to_submission": len_arr[0],
+            "submission_to_started": len_arr[1],
+            "file_transfer_time": len_arr[2],
+            "transfer_speed(b/s)": transfer_speed,
+            "transfer_speed(MB/s)": f_size/8/len_arr[2]/1024/1024,
+            "max_usage_percentage": transfer_speed/max_speed*100
+        }
+        #Appends the dictionary to our array of output dictionaries.
+        speed_info.append(info)
+    #Returns the speed info and the transfer array (in case it's been modified
+    #and had badly formatted stuff or incorrect request types removed)
+    for transfer in to_remove:
+        transfers.remove(transfer)
+    if len(transfers) > 0:
+        return compile_info(transfers, speed_info)
+    else:
+        return []
+
+
+#Based on Simplernerd's tutorial here: https://simplernerd.com/elasticsearch-scroll-python/
+def scroll(es, idx, body, scroll):
+    page = es.search(index=idx, body=body, scroll=scroll, size=search_size)
+    scroll_id = page['_scroll_id']
+    hits = page['hits']['hits']
+    while len(hits):
+        yield hits
+        page = es.scroll(scroll_id=scroll_id, scroll=scroll)
+        scroll_id = page['_scroll_id']
+        hits = page['hits']['hits']
+
+
+#End of function definitions
+
+#Start of main process
+
+
+#TODO: Add code to make sure we're not trying to search future dates
+
+#Runs a single search for the Elasticsearch client
+#data_in = client.search(index = index, body=es_template, size=search_size)["hits"]["hits"]
+
+f = open(output_file, "w+")
+#data = scroll(client, index, es_template, "2m")
+#if len(data) == 0:
+#    print("Warning: No finished transfers found in specified date range")
+#    f.write(json.dumps({"data" : error_out}, indent=2))
+#    f.close()
+#else:
+info = []
+for data in scroll(client, index, es_template, "2m"):
+    info += get_speeds(data)
+if len(info) == 0:
+    print("Error: No transfers fitting required parameters found")
+    f = open(output_file, "w+")
+    f.write(json.dumps({"data" : error_out}, indent=2))
+    f.close()
+else:
+    jres = json.dumps({"data": info}, indent=2)
+    f = open(output_file, "w+")
+    f.write(jres)
+    f.close()
+
+
+#processed_records = len(info)
+#print (f"{processed_records} records processed")
+
+#Makes sure that there is at least one transfer in the specified date range.
+#Skips the next step in the process if that's the case, which lets our next
+#bit of error handling exit the program.
+#if(len(data_in) == 0):
+#    print("Error: No finished transfers found in specified date range")
+#    speeds = []
+#else:
+#    speeds, data_in = get_speeds(data_in)
+
+#Checks that we have at least one valid transfer in the specified date range.
+#Writes a static error JSON to the output file if no transfers are found,
+#and then exits.
+#if(len(speeds) == 0):
+#    print("Error: No transfers fitting required parameters found")
+#    f = open(output_file, "w+")
+#    f.write(json.dumps({"data" : error_out}, indent=2))
+#    f.close()
+#    exit()
+
+
+#Compiles all of our information to JSON objects and then dumps them
+#as a single JSON object to our output file.
+#info = compile_info(data_in, speeds)
---
+++ src/App.js
@@ -0,0 +1,441 @@
+import React, { useState } from "react";
+import ReactTooltip from "react-tooltip";
+import ReactDOM from "react-dom";
+import Tooltip from "react-simple-tooltip";
+import DayPicker, { DateUtils } from 'react-day-picker';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Graticule,
+  ZoomableGroup,
+  Line,
+  Marker,
+} from "react-simple-maps";
+import "./App.css";
+// import siteData from "./data/duneSiteList.json"
+
+// var fs = require('fs');
+
+
+var resultsFound = false
+
+
+function dateFormatConverter(passedDate) {
+  const date = passedDate.toISOString().split('T')[0].replace(/-/g, "/");
+  return date
+}
+
+
+
+
+function checkIfResultsFound()
+{
+
+  // console.log("results found flag says: " + resultsFound)
+
+  if (resultsFound == undefined){
+    return
+  }
+
+  if (resultsFound){
+      return <p>Results Found</p>
+  }
+  else{
+    return <p>NO RESULTS FOUND</p>
+  }
+
+}
+
+
+//longitude first, then latitude
+
+const markers = [
+
+  { markerOffset: 1, otherName:"", name: "RAL_ECHO", coordinates: [1.2, 51.6] },
+  { markerOffset: 1, otherName:"", name: "CERN_PDUNE_CASTOR", coordinates: [6, 46] },
+  { markerOffset: 1, otherName:"", name: "PRAUGE", coordinates: [14.469, 50.123] },
+  { markerOffset: 1, otherName:"", name: "FNAL_DCACHE", coordinates: [-88.27, 41.84] },
+
+  { markerOffset: 1, otherName:"", name: "DUNE_US_BNL_SDCC", coordinates: [-72.876311, 40.86794] },
+  { markerOffset: 1, otherName:"", name: "DUNE_FR_CCIN2P3_XROOTD", coordinates: [4.87, 45.78] },
+  { markerOffset: 1, otherName:"", name: "NERSC", coordinates: [-122.272778, 37.871667] },
+
+  { markerOffset: 1, otherName:"", name: "Atlantis (Unknon Location Placeholder)!", coordinates: [-43, 32.6] },
+
+    { markerOffset: 1, otherName:"", name: "CERN_PDUNE_EOS", coordinates: [0, 0] },
+  { markerOffset: 1, otherName:"", name: "T3_US_NERSC", coordinates: [1, 0] },       //placeholder location 4-12-21 someone promised I'd get this, is it 2040 yet?
+  { markerOffset: 1, otherName:"", name: "DUNE_FR_CCIN2P3", coordinates: [2, 0] },   //placeholder location 4-12-21 someone promised I'd get this, is it 2040 yet?
+
+  //stuff commented out below has been found in the API results from CRIC API so I figure we should favor that
+
+  // { markerOffset: 1, name: "MANCHESTER", coordinates: [2.2, 53] },
+  // { markerOffset: 1, name: "LANCASTER", coordinates: [-2.74, 54.012] },
+  // { markerOffset: 1, name: "LIVERPOOL", coordinates: [-3, 53.4] },
+  // { markerOffset: 1, name: "NIKHEF", coordinates: [4.951, 52.3] },
+  // { markerOffset: 1, name: "IMPERIAL", coordinates: [0.17, 51.4] },
+  // { markerOffset: 1, name: "QMUL", coordinates: [-0.041, 51.523] },
+  // { markerOffset: 1, name: "RAL-PP", coordinates: [51.57, -1.31] },
+
+];
+
+
+const geoUrl =
+  "./world-110m.json";
+
+function App() {
+  const [transfers, settransfers] = useState([]);
+  const [individualSiteData, setIndividualSiteData] = useState([]);
+
+  const [dateRange, setDateRange] = useState({from: undefined, to: undefined});
+  const [savedStartDate, setSavedStartDate]=useState();
+  const [savedEndDate, setSavedEndDate]=useState();
+
+
+
+
+
+
+
+
+
+  const resetCalendarDateClick = () => {
+    setDateRange({from: undefined, to: undefined})
+  }
+
+
+
+
+
+
+  const handleDateClick = (day) => {
+
+    if (dateRange.to) {
+      resetCalendarDateClick()
+    }
+    else{
+      const range = DateUtils.addDayToRange(day, dateRange);
+      setDateRange(range)
+    }
+
+
+  }
+
+
+
+
+
+
+const parseSiteList = () => {
+
+  console.log("fetching DUNE site date from backend fermicloud129.fnal.gov:3001/getsites")
+  fetch("http://localhost:3001/getsites").then ((res) => res.json()).then((res) => {
+
+    //res.root.atp_site[0].$.latitude
+    // console.log(res.root.atp_site)
+    console.log("Dune Site list updated as of: " + res.root.last_update[0])
+
+    const mappedSites = res.root.atp_site.map((item) => {
+
+      var otherNameString = item.group[1].$.name
+      let re = new RegExp('[A-Z][A-Z]_');
+
+      if (re.test(otherNameString))
+      {
+        otherNameString = otherNameString.substring(3).toUpperCase();
+      }
+
+
+      return{
+        markerOffset: 1,
+        name: item.$.name.toUpperCase(),
+        otherName: otherNameString,
+        coordinates: [parseFloat(item.$.longitude),parseFloat(item.$.latitude)]
+
+      };
+    });
+
+    //append these to the existing hardcoded sites
+    mappedSites.forEach(x =>  markers.push(x))
+
+    console.log(mappedSites)
+    console.log(markers)
+    // console.log(res.root.atp_site[0].group[1].$.name)
+
+  });
+};
+
+
+
+
+  const action = () => {
+
+    resultsFound=false
+
+    setSavedStartDate(dateFormatConverter(dateRange.from))
+    setSavedEndDate(dateFormatConverter(dateRange.to))
+
+    var dateParameters = new URLSearchParams({"startDate": dateFormatConverter(dateRange.from), "endDate": dateFormatConverter(dateRange.to)});
+
+    console.log("fermicloud129.fnal.gov:3001/test?" + dateParameters.toString())
+
+    fetch("http://localhost:3001/test?" + dateParameters.toString())
+      .then((res) => res.json())
+      .then((res) => {
+
+        let allTransferedAmount = 0;
+
+        // console.log(res.data[0].name)
+
+        if (res.data[0].name!=="ERROR") {
+
+//TODO: modify this so that if the search fails we don't crash, maybe try/accept or if statement
+
+          const mappedTransfers = res.data.map((entry) => {
+            const sourceLocation = markers.find(
+              (location) => entry.source === location.name
+            );
+            const destinationLocation = markers.find(
+              (location) => entry.destination === location.name
+            );
+            const speedInMB = parseFloat(entry["transfer_speed(MB/s)"]).toFixed(
+              2
+            );
+            allTransferedAmount += entry.file_size;
+
+            return {
+              from: sourceLocation.name,
+              to: destinationLocation.name,
+              fromCoord: sourceLocation.coordinates,
+              toCoord: destinationLocation.coordinates,
+              speedInMB: speedInMB,
+            };
+          });
+
+
+
+          allTransferedAmount /= 1048576; //adjusting to mb
+
+          settransfers(mappedTransfers);
+
+          const collectionOfSiteObjects = markers.map((marker) => {
+            return {
+              ...marker,
+              totalSent: 0,
+              totalReceived: 0,
+            };
+          });
+
+          collectionOfSiteObjects.forEach((entry) => {
+            res.data
+              .filter((jsonThing) => {
+                return jsonThing.source === entry.name;
+              })
+              .forEach((item, i) => {
+                entry.totalSent += item.file_size / 1048576; //dividing the total bytes into megabytes 1024 b to kb, 1024 kb to mb
+              });
+
+            res.data
+              .filter((jsonThing) => {
+                return jsonThing.destination === entry.name;
+              })
+              .forEach((item, i) => {
+                entry.totalReceived += item.file_size / 1048576; //dividing the total bytes into megabytes 1024 b to kb, 1024 kb to mb
+              });
+
+            entry.fractionOfDataSent = entry.totalSent / allTransferedAmount;
+            entry.fractionOfDataReceived =
+              entry.totalReceived / allTransferedAmount;
+
+            entry.totalSent = parseFloat(entry.totalSent).toFixed(2);
+            entry.totalReceived = parseFloat(entry.totalReceived).toFixed(2);
+            entry.fractionOfDataSent = parseFloat(
+              entry.fractionOfDataSent
+            ).toFixed(4);
+          });
+
+          resultsFound=true;
+          console.log("Results found:")
+          console.log(collectionOfSiteObjects);
+
+          setIndividualSiteData(collectionOfSiteObjects);
+
+
+        }
+        else {
+          resultsFound=false;
+          console.log("No results returned for DUNE transfers")
+          console.log(resultsFound)
+        }
+
+
+      });
+  };
+
+
+
+
+
+
+
+
+
+
+  const [tooltip, setTooltip] = useState("");
+  const [position, setPosition] = useState({ coordinates: [0, 0], zoom:   1 });
+
+
+
+
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DUNE Network Activity Monitor - Alpha</h1>
+      </header>
+
+      <div class="basicRow">
+        <div class="basicColumn">
+          <div id={"map"}>
+            <ComposableMap data-tip="">
+              <ZoomableGroup zoom={3} center={[-34, 34]}>
+
+                <Geographies geography={geoUrl}>
+                  {({ geographies }) =>
+                    geographies.map((geo) => (
+                      <Geography key={geo.rsmKey} geography={geo} fill="#9998A3" stroke="#EAEAEC" />
+                    ))
+                  }
+                </Geographies>
+                {transfers.map((oneOfThem, i) => {
+                  return (
+                    <Line
+                      key={i}
+                      to={oneOfThem.toCoord}
+                      from={oneOfThem.fromCoord}
+                      stroke="#F53"
+                      strokeWidth={1}
+                      onMouseEnter={() => {
+                        // setTooltip(`Last AVG speed: ${oneOfThem.speedInMB} MB/s`);       //need to consider what, if any, we want to put in tooltip over transfer line
+                      }}
+                      onMouseLeave={() => {
+                        setTooltip("");
+                      }}
+                    />
+                  );
+                })}
+                //could add another line here ^ to show ration of send vs
+                recieve between individual sites but it's one within another not
+                side by side so doesn't look great.
+                {individualSiteData.map(
+                  (
+                    {
+                      name,
+                      coordinates,
+                      markerOffset,
+                      totalSent,
+                      fractionOfDataSent,
+                      fractionOfDataReceived,
+                    },
+                    i
+                  ) => (
+                    <Marker key={i} coordinates={coordinates}
+                    onClick={() => {
+                      //alert("click action here");
+                      //alert("radius click")
+                    }}
+                    >
+                      <circle r={1} fill="rgba(255,255,255,1)" />
+                      <circle
+                        r={40 * fractionOfDataSent}
+                        fill="rgba(87,235,51,0.4)"
+                      />{" "}
+                      //send fraction circle
+                      <circle
+                        r={40 * fractionOfDataReceived}
+                        fill="rgba(12,123,220,0.4)"
+                      />{" "}
+                      //recieve fraction circle
+                    </Marker>
+                  )
+                )}
+                {individualSiteData.map(
+                  (
+                    {
+                      name,
+                      coordinates,
+                      markerOffset,
+                      totalSent,
+                      totalReceived,
+                      fractionOfDataSent,
+                      fractionOfDataReceived,
+                    },
+                    i
+                  ) => (
+                    <Marker
+                      key={i}
+                      coordinates={coordinates}
+                      onClick={() => {
+                        //alert("center click");
+                      }}
+                      onMouseEnter={() => {
+                        setTooltip(
+                          `${name}<br> TX: ${totalSent} MB <br>  RX: ${totalReceived} MB`
+                        );
+                      }}
+                      onMouseLeave={() => {
+                        setTooltip("");
+                      }}
+                    >
+                      <circle r={1} fill="rgba(75,0,146,1)" />
+                    </Marker>
+                  )
+                )}
+              </ZoomableGroup>
+            </ComposableMap>
+          </div>
+          <ReactTooltip html={true}>{tooltip}</ReactTooltip>
+
+        </div>
+
+          <p>
+
+
+              <button disabled={!dateRange.from} className="link" onClick={resetCalendarDateClick}>
+                Reset Selected Dates
+              </button>
+
+              <button onClick={parseSiteList}>
+                Do site stuff
+              </button>
+
+          </p>
+
+          <DayPicker
+            selectedDays={[dateRange.from, dateRange]}
+            onDayClick={handleDateClick}
+          />
+
+
+
+        <div class="basicColumn">
+
+          {dateRange.to && <button onClick={action}>Get DUNE Transfer Data</button>}
+
+          {checkIfResultsFound()}
+
+          {resultsFound && <p>Showing Transfers from {savedStartDate} to {savedEndDate}</p>}
+
+          {resultsFound && JSON.stringify(transfers)}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
