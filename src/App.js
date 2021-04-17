beeb5600a65b2641410c56942f9e15b7a47728e4
cathulhu <iheartpugs89@gmail.com>
2021-04-17 05:44:58 -0700

    translated to first steps of new interface, minor adjustment to features

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Collapse, Button, CardBody, Card  } from 'reactstrap';
 import ReactTooltip from "react-tooltip";
 import ReactDOM from "react-dom";
 import Tooltip from "react-simple-tooltip";
@@ -15,6 +16,7 @@ import {
   Marker,
 } from "react-simple-maps";
 import "./App.css";
+import './css/bootstrap.min.css';
 // import siteData from "./data/duneSiteList.json"
 
 // var fs = require('fs');
@@ -83,8 +85,7 @@ const markers = [
 
 const baseUrlBackend="http://fermicloud129.fnal.gov:3001"
 
-const geoUrl =
-  "./world-110m.json";
+const geoUrl = "./world-110m.json";
 
 function App() {
   const [transfers, settransfers] = useState([]);
@@ -179,6 +180,11 @@ const parseSiteList = () => {
   const parseTransfers = () => {
 
     resultsFound=false
+
+    if (dateRange.to === undefined){
+      dateRange.to = dateRange.from
+    }
+
 
     setSavedStartDate(dateFormatConverter(dateRange.from))
     setSavedEndDate(dateFormatConverter(dateRange.to))
@@ -345,138 +351,24 @@ const parseSiteList = () => {
 
 
 
-
-
-
+  const proccessTransferAndCollapse = () => {
+    parseSiteList()
+    toggle()
+  }
 
 
 
   const [tooltip, setTooltip] = useState("");
   const [position, setPosition] = useState({ coordinates: [0, 0], zoom:   1 });
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchResultStatus, setSearchResultStatus ] = useState();    //TODO actually get this working so empty text returned unless search complete, then return results found or not
 
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
 
 
 
-    <div className="App">
-      <header className="App-header">
-        <h1>DUNE Network Activity Monitor - Alpha</h1>
-      </header>
-
-
-      <div class="basicRow">
-        <div class="basicColumn">
-          <div id={"map"}>
-            <ComposableMap data-tip="">
-              <ZoomableGroup zoom={3} center={[-34, 34]}>
-
-                <Geographies geography={geoUrl}>
-                  {({ geographies }) =>
-                    geographies.map((geo) => (
-                      <Geography key={geo.rsmKey} geography={geo} fill="#9998A3" stroke="#EAEAEC" />
-                    ))
-                  }
-                </Geographies>
-                {transfers.map((oneOfThem, i) => {
-                  return (
-                    <Line
-                      key={i}
-                      to={oneOfThem.toCoord}
-                      from={oneOfThem.fromCoord}
-                      stroke="#F53"
-                      strokeWidth={1}
-                      onMouseEnter={() => {
-                        // setTooltip(`Last AVG speed: ${oneOfThem.speedInMB} MB/s`);       //need to consider what, if any, we want to put in tooltip over transfer line
-                      }}
-                      onMouseLeave={() => {
-                        setTooltip("");
-                      }}
-                    />
-                  );
-                })}
-                //could add another line here ^ to show ration of send vs recieve between individual sites but it's one within another not side by side so doesn't look great.
-                {individualSiteData.map(
-                  (
-                    {
-                      name,
-                      coordinates,
-                      markerOffset,
-                      totalSent,
-                      fractionOfDataSent,
-                      fractionOfDataReceived,
-                    },
-                    i
-                  ) => (
-                    <Marker key={i} coordinates={coordinates}
-                    onClick={() => {
-                      //alert("click action here");
-                      //alert("radius click")
-                    }}
-                    >
-                      <circle r={1} fill="rgba(255,255,255,1)" />
-                      <circle
-                        r={40 * fractionOfDataSent}
-                        fill="rgba(87,235,51,0.4)"
-                      />{" "}
-                      //send fraction circle
-                      <circle
-                        r={40 * fractionOfDataReceived}
-                        fill="rgba(12,123,220,0.4)"
-                      />{" "}
-                      //recieve fraction circle
-                    </Marker>
-                  )
-                )}
-                {individualSiteData.map(
-                  (
-                    {
-                      name,
-                      coordinates,
-                      markerOffset,
-                      totalSent,
-                      totalReceived,
-                      fractionOfDataSent,
-                      fractionOfDataReceived,
-                    },
-                    i
-                  ) => (
-                    <Marker
-                      key={i}
-                      coordinates={coordinates}
-                      onClick={() => {
-                        //alert("center click");
-                      }}
-                      onMouseEnter={() => {
-                        setTooltip(
-                          `${name}<br> TX: ${totalSent} MB <br>  RX: ${totalReceived} MB`
-                        );
-                      }}
-                      onMouseLeave={() => {
-                        setTooltip("");
-                      }}
-                    >
-                      <circle r={1} fill="rgba(75,0,146,1)" />
-                    </Marker>
-                  )
-                )}
-              </ZoomableGroup>
-            </ComposableMap>
-          </div>
-          <ReactTooltip html={true}>{tooltip}</ReactTooltip>
-
-        </div>
-
-
-
-
-
-
-
-        <DayPicker
-          selectedDays={[dateRange.from, dateRange]}
-          onDayClick={handleDateClick}
-        />
 
 
 
@@ -486,26 +378,300 @@ const parseSiteList = () => {
 
 
 
-      </div>
-      <div class="basicRow">
-
-      {dateRange.to && <button onClick={parseSiteList}>Get DUNE Transfer Data</button>}
-
-      <button disabled={!dateRange.from} className="link" onClick={resetCalendarDateClick}>
-        Reset Selected Dates
-      </button>
-
-      {checkIfResultsFound()}
 
 
-      </div>
-      <div class="basicRow">
-      <div class="basicColumn">
-        {resultsFound && <p>Showing Transfers from {savedStartDate} to {savedEndDate}</p>}
-        {resultsFound && JSON.stringify(transfers)}
-      </div>
-      </div>
+
+        <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-md-12">
+    			<div class="row">
+    				<div class="col-md-12">
+    					<div class="page-header">
+    						<h1>
+    							Dune Data Thing!
+    						</h1>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="row"id="mapTitleAnStatusRow">
+    				<div class="col-md-9" id="mainSectionCol">
+    					<div class="row" id="mapTitleRow">
+    						<div class="col-md-9">
+    							<div class="page-header">
+    								<h5>
+    									Interactive Transfer Visualization Map
+    								</h5>
+    							</div>
+    						</div>
+    						<div class="col-md-3" id="statusSectionCol">
+
+    							 <p>Progress par / status here</p>
+                   {checkIfResultsFound()}
+    							        <div class="progress">
+    								<div class="progress-bar w-75">
+    								</div>
+    							</div>
+
+    						</div>
+
+    					</div>
+    					<div class="row" id="legendRow">
+    						<div class="col-md-12">
+
+                  <div class="row">
+                    <p>Legend </p>
+                  </div>
+
+                  <div class="row">
+
+                <div class="col-sm-2">
+                  <h5>Data Sent</h5>
+                  <svg height="100" width="100">
+                    <circle cx="50" cy="50" r="40" stroke="black" stroke-width="0" fill="rgba(0,235,51,0.4)" />
+                    Sorry, your browser does not support inline SVG.
+                  </svg>
+                </div>
+                <div class="col-sm-2">
+                  <h5>Data Received</h5>
+                  <svg height="100" width="100">
+                    <circle cx="50" cy="50" r="40" stroke="black" stroke-width="0" fill="rgba(12,123,220,0.4)" />
+                    Sorry, your browser does not support inline SVG.
+                  </svg>
+                </div>
+                <div class="col-sm-2">
+                <h5>Dune Institution</h5>
+                <svg height="100" width="100">
+                  <circle cx="50" cy="50" r="10" stroke="black" stroke-width="0" fill="rgba(75,0,146,1)" />
+                  Sorry, your browser does not support inline SVG.
+                </svg>
+                </div>
+                <div class="col-sm-2">
+                <h5>Transfer Path</h5>
+
+                <svg viewBox = "0 0 100 40" version = "1.1">
+                    <line x1 = "3" y1 = "20" x2 = "75" y2 = "20" stroke = "black" stroke-width = "3" stroke = "#F53"/>
+                </svg>
+                </div>
+                <div class="col-md-4" id="legendDescriptionCol">
+                  <p>basic description of the map and how it works here</p>
+                </div>
+
+                  </div>
+
+    						</div>
+    					</div>
+    					<div class="row" id="mapRow">
+    						<div class="col-md-12">
+                  <p> map here</p>
+
+
+
+
+
+
+                  <div id={"map"}>
+                          <ComposableMap data-tip="">
+                            <ZoomableGroup zoom={3} center={[-34, 34]}>
+
+                              <Geographies geography={geoUrl}>
+                                {({ geographies }) =>
+                                  geographies.map((geo) => (
+                                    <Geography key={geo.rsmKey} geography={geo} fill="#9998A3" stroke="#EAEAEC" />
+                                  ))
+                                }
+                              </Geographies>
+                              {transfers.map((oneOfThem, i) => {
+                                return (
+                                  <Line
+                                    key={i}
+                                    to={oneOfThem.toCoord}
+                                    from={oneOfThem.fromCoord}
+                                    stroke="#F53"
+                                    strokeWidth={1}
+                                    onMouseEnter={() => {
+                                      // setTooltip(`Last AVG speed: ${oneOfThem.speedInMB} MB/s`);       //need to consider what, if any, we want to put in tooltip over transfer line
+                                    }}
+                                    onMouseLeave={() => {
+                                      setTooltip("");
+                                    }}
+                                  />
+                                );
+                              })}
+                              //could add another line here ^ to show ration of send vs recieve between individual sites but it's one within another not side by side so doesn't look great.
+                              {individualSiteData.map(
+                                (
+                                  {
+                                    name,
+                                    coordinates,
+                                    markerOffset,
+                                    totalSent,
+                                    fractionOfDataSent,
+                                    fractionOfDataReceived,
+                                  },
+                                  i
+                                ) => (
+                                  <Marker key={i} coordinates={coordinates}
+                                  onClick={() => {
+                                    //alert("click action here");
+                                    //alert("radius click")
+                                  }}
+                                  >
+                                    <circle r={1} fill="rgba(255,255,255,1)" />
+                                    <circle
+                                      r={40 * fractionOfDataSent}
+                                      fill="rgba(87,235,51,0.4)"
+                                    />{" "}
+                                    //send fraction circle
+                                    <circle
+                                      r={40 * fractionOfDataReceived}
+                                      fill="rgba(12,123,220,0.4)"
+                                    />{" "}
+                                    //recieve fraction circle
+                                  </Marker>
+                                )
+                              )}
+                              {individualSiteData.map(
+                                (
+                                  {
+                                    name,
+                                    coordinates,
+                                    markerOffset,
+                                    totalSent,
+                                    totalReceived,
+                                    fractionOfDataSent,
+                                    fractionOfDataReceived,
+                                  },
+                                  i
+                                ) => (
+                                  <Marker
+                                    key={i}
+                                    coordinates={coordinates}
+                                    onClick={() => {
+                                      //alert("center click");
+                                    }}
+                                    onMouseEnter={() => {
+                                      setTooltip(
+                                        `${name}<br> TX: ${totalSent} MB <br>  RX: ${totalReceived} MB`
+                                      );
+                                    }}
+                                    onMouseLeave={() => {
+                                      setTooltip("");
+                                    }}
+                                  >
+                                    <circle r={1} fill="rgba(75,0,146,1)" />
+                                  </Marker>
+                                )
+                              )}
+                            </ZoomableGroup>
+                          </ComposableMap>
+                        </div>
+                        <ReactTooltip html={true}>{tooltip}</ReactTooltip>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    						</div>
+    					</div>
+    					<div class="row" id="listRow">
+    						<div class="col-md-12">
+                  <p>-collapsable- list of raw data results here</p>
+
+                  {resultsFound && <p>Showing Transfers from {savedStartDate} to {savedEndDate}</p>}
+                  {resultsFound && JSON.stringify(transfers)}
+
+    						</div>
+    					</div>
+    				</div>
+    				<div class="col-md-3 position-fixed" id="fixedRightCol">
+            <div class="rightSideFixed">
+    					<div class="row" id="searchButtonRow">
+    						<div class="col-md-12" id="">
+                  <div class="row">
+                  <div>
+                    <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>New Search</Button>
+                    </div>
+                  </div>
+    						</div>
+    					</div>
+              <Collapse isOpen={isOpen}>
+    					<div class="row" id="calendarRow">
+
+
+
+
+    						<div class="col-md-12">
+
+
+
+                <div class="card" >
+                  <div class="card-body">
+                    <div class="card-header">
+                      <h5 class="card-title">Transfer Date Range</h5>
+                      <h6 class="card-subtitle mb-2 text-muted"></h6>
+                      <p class="card-text">Select a date (or range) below.</p>
+                    </div>
+
+                    <DayPicker selectedDays={[dateRange.from, dateRange]} onDayClick={handleDateClick}/>
+
+                    <div class="row" id="calendarButtonRow">
+                    <div class="col-md-12">
+                      <div class="btn-group" role="group">
+
+                        <Button color="primary" disabled={!dateRange.from} onClick={proccessTransferAndCollapse}>Get Transfers</Button>
+
+                        <Button color="primary" disabled={!dateRange.from} onClick={resetCalendarDateClick}>
+                          Reset Selected Dates
+                        </Button>
+
+                      </div>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+                  </div>
+
+    					</div>
+                              </Collapse>
+    					<div class="row" id="detailRow">
+    						<div class="col-md-12">
+                <p>Site detail info here</p>
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
     </div>
+</div>
+
+
   );
 }
 
